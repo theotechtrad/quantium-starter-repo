152faaf4f44a2b9792b2802fcb7ff4c812e08f7b
theotechtrad <theotechtrad@gmail.com>
2026-02-16 15:28:20 +0530

    Add Vercel deployment configuration

--- app.py
+++ app.py
@@ -165,6 +165,9 @@ def update_chart(selected_region):
     return fig
 
 # Run the app
+# For Vercel deployment
+server = app.server
+
 if __name__ == '__main__':
     print("Starting Dash app...")
     print("Go to: http://127.0.0.1:8050/")
